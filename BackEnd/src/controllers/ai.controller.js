1fb5b9b91465f32806c8b2543f38100db804fd7b
Aadrika11 <aadrika.works.11@gmail.com>
2025-04-23 22:25:52 +0530

    first commit

---
+++ BackEnd/src/controllers/ai.controller.js
@@ -0,0 +1,9 @@
+const aiService= require("./../sevices/ai.service")
+module.exports.getReview=async (req,res)=>{
+    const code=req.body.code;
+    if(!code){
+        return res.status(400).send("Prompt is required");
+    }
+    const response=await aiService(code);
+    res.send(response);
+}
